a036b15ff6013e018796ba6749c3ec3e8a1ac02f
RomaTheSun <pryimak.roma@gmail.com>
2024-09-30 12:51:01 +0300

    task8

--- AT/src/test/java/task8/Task8Test.java
+++ AT/src/test/java/task8/Task8Test.java
@@ -1,7 +1,6 @@
 package task8;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;  // TestNG
+import org.testng.annotations.*;
 import task2.Task2;
 
 
@@ -12,28 +11,58 @@ public class Task8Test {
     void setUp() {
         task2 = new Task2();
     }
-    @Test
-    public void isDigitTest() {
-        Character inputChar = '2';
-        Boolean expectedResult = true;
+
+    @DataProvider(name = "digitProvider")
+    public Object[][] digitProvider() {
+        return new Object[][]{
+                {'2', true},
+                {'a', false},
+                {'%', false}
+        };
+    }
+
+    @Test(dataProvider = "digitProvider")
+    public void isDigitTest(Character inputChar, Boolean expectedResult) {
         Boolean actualResult = task2.isDigit(inputChar);
         Assert.assertEquals(actualResult, expectedResult, "Unexpected result");
     }
-    @Test
-    public void isLetter() {
-        Character input = '2';
-        Boolean expectedResult = false;
+
+    @DataProvider(name = "letterProvider")
+    public Object[][] letterProvider() {
+        return new Object[][]{
+                {'2', false},
+                {'a', true},
+                {'$', false}
+        };
+    }
+
+    @Test(dataProvider = "letterProvider")
+    public void isLetterTest(Character input, Boolean expectedResult) {
         Boolean actualResult = task2.isLetter(input);
         Assert.assertEquals(actualResult, expectedResult, "Unexpected result");
     }
-    @Test
-    public void isUpperCase() {
-        Character input = 'd';
-        Character expectedResult = 'D';
+
+    @DataProvider(name = "upperCaseProvider")
+    public Object[][] upperCaseProvider() {
+        return new Object[][]{
+                {'d', 'D'},
+                {'A', 'A'},
+                {'z', 'Z'}
+        };
+    }
+
+    @Test(dataProvider = "upperCaseProvider")
+    public void isUpperCaseTest(Character input, Character expectedResult) {
         Character actualResult = task2.isUpperCase(input);
         Assert.assertEquals(actualResult, expectedResult, "Unexpected result");
     }
 
+    @Parameters({"input", "expected"})
+    @Test
+    public void parameterizedTest(@Optional("a") char input, @Optional("true") boolean expected) {
+        boolean actualResult = task2.isLetter(input);
+        Assert.assertEquals(actualResult, expected, "Unexpected result for input: " + input);
+    }
 }
 
 //1: Create 3 simple unit tests for Task_2 (modify your code to have 3 different methods in Task_2 solving if needed).
